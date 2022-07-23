589593d8558fe826494a7c9afba8cb7ea774c39f
Mathieu Payrol <mathieu.payrol@gmail.com>
2022-07-23 18:27:27 +0000

    Adjust Push workflow and associated tests (#9)

--- tests/test_init.py
+++ tests/test_init.py
@@ -4,7 +4,7 @@ import pytest
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
 from custom_components.intex_spa import (
-    BlueprintDataUpdateCoordinator,
+    IntexSpaDataUpdateCoordinator,
     async_reload_entry,
     async_setup_entry,
     async_unload_entry,
@@ -19,25 +19,27 @@ from .const import MOCK_CONFIG
 # Home Assistant using the pytest_homeassistant_custom_component plugin.
 # Assertions allow you to verify that the return value of whatever is on the left
 # side of the assertion matches with the right side.
-async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
+async def test_setup_unload_and_reload_entry(
+    hass, bypass_update_info, bypass_update_data
+):
     """Test entry setup and unload."""
     # Create a mock entry so we don't have to go through config flow
     config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
 
     # Set up the entry and assert that the values set during setup are where we expect
-    # them to be. Because we have patched the BlueprintDataUpdateCoordinator.async_get_data
-    # call, no code from custom_components/intex_spa/api.py actually runs.
+    # them to be. Because we have patched the IntexSpa.async_update_data
+    # and IntexSpa.async_update_info calls, no code from IntexSpa actually runs.
     assert await async_setup_entry(hass, config_entry)
     assert DOMAIN in hass.data and config_entry.entry_id in hass.data[DOMAIN]
     assert (
-        type(hass.data[DOMAIN][config_entry.entry_id]) == BlueprintDataUpdateCoordinator
+        type(hass.data[DOMAIN][config_entry.entry_id]) == IntexSpaDataUpdateCoordinator
     )
 
     # Reload the entry and assert that the data from above is still there
     assert await async_reload_entry(hass, config_entry) is None
     assert DOMAIN in hass.data and config_entry.entry_id in hass.data[DOMAIN]
     assert (
-        type(hass.data[DOMAIN][config_entry.entry_id]) == BlueprintDataUpdateCoordinator
+        type(hass.data[DOMAIN][config_entry.entry_id]) == IntexSpaDataUpdateCoordinator
     )
 
     # Unload the entry and verify that the data has been removed
@@ -45,12 +47,12 @@ async def test_setup_unload_and_reload_entry(hass, bypass_get_data):
     assert config_entry.entry_id not in hass.data[DOMAIN]
 
 
-async def test_setup_entry_exception(hass, error_on_get_data):
+async def test_setup_entry_exception(hass, error_on_update_info, error_on_update_data):
     """Test ConfigEntryNotReady when API raises an exception during entry setup."""
     config_entry = MockConfigEntry(domain=DOMAIN, data=MOCK_CONFIG, entry_id="test")
 
     # In this case we are testing the condition where async_setup_entry raises
     # ConfigEntryNotReady using the `error_on_get_data` fixture which simulates
     # an error.
-    with pytest.raises(ConfigEntryNotReady):
+    with pytest.raises(NotImplementedError):
         assert await async_setup_entry(hass, config_entry)
