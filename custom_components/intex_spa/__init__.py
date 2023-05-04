c483db801975b7ae53c18fae5932948cd195ee0a
FredericMa <FredericMa@users.noreply.github.com>
2023-05-04 21:19:41 +0200

    Fix init on 2023.5

--- custom_components/intex_spa/__init__.py
+++ custom_components/intex_spa/__init__.py
@@ -45,7 +45,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await coordinator.async_update_info()
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     entry.async_on_unload(entry.add_update_listener(async_reload_entry))
 
